e327cee080bbd37b0b0077a5c251ee9a4704c98b
Rod Hinman <rod@auroraresearch.com>
2022-06-25 15:29:12 -1000

    [pixy] Getting blocks and splitting them according to shape.

--- code/pixyvision/robot.py
+++ code/pixyvision/robot.py
@@ -20,8 +20,8 @@ class MAKORobot(wpilib.TimedRobot):
         self.pixy.init() # Need to call init() to start communication with the Pixy2.
         print('FPS: {}'.format(self.pixy.getFPS()))
         #print('lamp white: {}'.format(self.pixy.setLamp(1,0)))
-        print('led rgb: {}'.format(self.pixy.setLED(red=0,green=255,blue=0)))
-        print('lamp on rgb: {}'.format(self.pixy.setLamp(0,1)))
+        # print('led rgb: {}'.format(self.pixy.setLED(red=0,green=255,blue=0)))
+        # print('lamp on rgb: {}'.format(self.pixy.setLamp(0,1)))
 
     def disabledInit(self):
         """This function gets called once when the robot is disabled.
@@ -53,29 +53,34 @@ class MAKORobot(wpilib.TimedRobot):
         # The timer's hasPeriodPassed() method returns true if the time has passed, and updates
         # the timer's internal "start time".  This period is 1.0 seconds.
         if self.print_timer.hasPeriodPassed(1.0):
-            # Send a string representing the red component to a field called 'DB/String 0' on the SmartDashboard.
-            # The default driver station dashboard's "Basic" tab has some pre-defined keys/fields
-            # that it looks for, which is why I chose these.
-            # Send info to the logger and thus console.
-            # Note the string format: the part in {} gets replaced by the value of the variable
-            # user_value, and is formatted as a floating point (the "f"), with 4 digits and 2 digits
-            # after the decimal place. https://docs.python.org/3/library/string.html#formatstrings
-            #self.logger.info('Another value is {:4.2f}'.format(0.33))
+            # See if Pixy has found any color connected components with signature 1, up to 10.
             num_blocks = self.pixy.getCCC().getBlocks(wait=False, sigmap=0x01, maxBlocks=10)
+            wpilib.SmartDashboard.putString('DB/String 0', 'num blocks: {}'.format(num_blocks))
             if num_blocks > 0:
                 blocks = self.pixy.getCCC().getBlockCache()
-#                print('blocks: {} {} {}'.format(num_blocks, len(blocks), blocks[0].toString()))
-                wpilib.SmartDashboard.putString('DB/String 0', 'pos x {:3d}'.format(blocks[0].getX()))
-                wpilib.SmartDashboard.putString('DB/String 1', 'pos y {:3d}'.format(blocks[0].getY()))
-                wpilib.SmartDashboard.putString('DB/String 5', 'width  {:3d}'.format(blocks[0].getWidth()))
-                wpilib.SmartDashboard.putString('DB/String 6', 'height {:3d}'.format(blocks[0].getHeight()))
-                wpilib.SmartDashboard.putString('DB/String 2', 'index {:3d}'.format(blocks[0].getIndex()))
-                wpilib.SmartDashboard.putString('DB/String 7', 'age {:3d}'.format(blocks[0].getAge()))
-                wpilib.SmartDashboard.putString('DB/String 4', 'num blocks {:3d}'.format(num_blocks))
-                if num_blocks > 1:
-                    wpilib.SmartDashboard.putString('DB/String 9', '2nd block x,y {:3d} {:3d}'.format(blocks[1].getX(), blocks[1].getY()))
-            else:
-                print('blocks: {}'.format(num_blocks))
+                wpilib.SmartDashboard.putString('DB/String 1', 'posx,y  sizex,y [asp] idx')
+
+                # Split the list of blocks by their aspect ratio.  They are already sorted largest area to smallest by Pixy.
+                accepted_blocks = []
+                rejected_blocks = []
+                for block in blocks:
+                    if block.getHeight() / block.getWidth() >= 2.0:
+                        accepted_blocks.append(block)
+                    else:
+                        rejected_blocks.append(block)
+                
+                # Print accepted and then rejected blocks to the smart dashboard.
+                for (i, block) in enumerate(accepted_blocks):
+                    wpilib.SmartDashboard.putString('DB/String {}'.format(i+2), 
+                    '{:3d},{:3d}  {:3d},{:3d} [{:1.2f}] {}'.format(block.getX(), block.getY(), block.getWidth(), block.getHeight(), block.getHeight()/block.getWidth(), block.getIndex()))
+
+                for (i, block) in enumerate(rejected_blocks):
+                    wpilib.SmartDashboard.putString('DB/String {}'.format(i+len(accepted_blocks)+2), 
+                    '*{:3d},{:3d}  {:3d},{:3d} [{:1.2f}] {}'.format(block.getX(), block.getY(), block.getWidth(), block.getHeight(), block.getHeight()/block.getWidth(), block.getIndex()))
+
+            # Clear any unused lines in the smart dashboard.
+            for i in range(num_blocks+2, 10):
+                wpilib.SmartDashboard.putString('DB/String {}'.format(i), '')
 
 # The following little bit of code allows us to run the robot program.
 # In Python, the special variable __name__ contains the name of the module that it is in,
